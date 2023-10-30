d68f8636f5ad0830e448734b5a386f7ba9292378
Kemal Oğuzhan Yeyit <kemaloguzhanyeyit@gmail.com>
2023-10-30 10:58:36 +0300

    CRUD operations added without defensive code princibles

--- rentACar/src/main/java/kodlama/io/rentACar/business/abstracs/BrandService.java
+++ rentACar/src/main/java/kodlama/io/rentACar/business/abstracs/BrandService.java
@@ -3,11 +3,16 @@ package kodlama.io.rentACar.business.abstracs;
 import java.util.List;
 
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.requests.UpdateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.responses.GetByIdBrandResponse;
 
 public interface BrandService {
 	List<GetAllBrandsResponse> getAll();
+	GetByIdBrandResponse getByID(int id);
 	
 	void add(CreateBrandRequest createBrandRequest);
+	void update(UpdateBrandRequest updateBrandRequest);
+	void delete(int id);
 }
  
